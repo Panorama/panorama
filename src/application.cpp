c71d125cdd14967ea864bdc511f66e5545d0e807
David Flemström <david.flemstrom@gmail.com>
2009-12-29 22:14:24 +0100

    Made application comparison more failsafe.

--- src/application.cpp
+++ src/application.cpp
@@ -2,6 +2,5 @@
 
 bool Application::operator ==(const Application &app) const
 {
-    //Each .desktop file is loaded only once, so we compare by file
-    return app.relatedFile == this->relatedFile;
+    return app.id == this->id;
 }

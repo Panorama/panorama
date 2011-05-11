54b21d536825a27b4dd2ddc9dedbbda50189fa6e
David Flemström <david.flemstrom@gmail.com>
2011-05-11 16:43:08 +0200

    Updated DesktopFile to use the new model

--- panorama/src/desktopfile.h
+++ panorama/src/desktopfile.h
@@ -1,6 +1,8 @@
 #ifndef DESKTOPFILE_H
 #define DESKTOPFILE_H
 
+#include "panoramainternal.h"
+
 #include <QString>
 #include <QFile>
 #include <QLocale>
@@ -13,29 +15,27 @@
 #include "pndscanner.h"
 #include "pnd.h"
 
+class DesktopFilePrivate;
+
 /**
  * Represents a FDF .desktop file
  */
 class DesktopFile
 {
+    PANORAMA_DECLARE_PRIVATE(DesktopFile)
 public:
     /**
      * Constructs a new DesktopFile instance, using the specified .desktop
      * file
      */
     DesktopFile(const QString &file);
+    ~DesktopFile();
 
     /**
      * Reads the contents of the attached .desktop file into an Application
      * instance
      */
     Application readToApplication();
-
-private:
-    static QString readLocalized(const QMap<QString, QVariant> &map, const QString &fieldName);
-    static QMap<QString, QVariant> parseDesktop(const QString &file);
-
-    const QString _file;
 };
 
 #endif // DESKTOPFILE_H

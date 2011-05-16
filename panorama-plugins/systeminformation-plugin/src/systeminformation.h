246ca1ebd9fa9c740862af6197522dca5c4f96bc
David Flemström <david.flemstrom@gmail.com>
2011-05-16 20:45:51 +0200

    Moved SystemInformation into an attached object

--- panorama-plugins/systeminformation-plugin/src/systeminformation.h
+++ panorama-plugins/systeminformation-plugin/src/systeminformation.h
@@ -1,100 +1,18 @@
 #ifndef SYSTEMINFORMATION_H
 #define SYSTEMINFORMATION_H
 
-#include "panoramainternal.h"
-
-#include <QObject>
 #include <qdeclarative.h>
 
-class SystemInformationPrivate;
+#include "systeminformationattached.h"
 
 class SystemInformation : public QObject
 {
-    Q_OBJECT
-    Q_PROPERTY(int cpu      READ cpu        NOTIFY cpuUpdated)
-    Q_PROPERTY(int usedCpu  READ usedCpu    NOTIFY usedCpuUpdated)
-    Q_PROPERTY(int ram      READ ram        NOTIFY ramUpdated)
-    Q_PROPERTY(int usedRam  READ usedRam    NOTIFY usedRamUpdated)
-    Q_PROPERTY(int swap     READ swap       NOTIFY swapUpdated)
-    Q_PROPERTY(int usedSwap READ usedSwap   NOTIFY usedSwapUpdated)
-    Q_PROPERTY(int sd1      READ sd1        NOTIFY sd1Updated)
-    Q_PROPERTY(int usedSd1  READ usedSd1    NOTIFY usedSd1Updated)
-    Q_PROPERTY(int sd2      READ sd2        NOTIFY sd2Updated)
-    Q_PROPERTY(int usedSd2  READ usedSd2    NOTIFY usedSd2Updated)
-    PANORAMA_DECLARE_PRIVATE(SystemInformation)
 public:
     explicit SystemInformation(QObject *parent = 0);
-    ~SystemInformation();
 
-    /**
-     * Returns a number representing the maximum CPU time for any given
-     * time span. Use this as a fraction with "usedCpu" to get the CPU-usage in
-     * percent.
-     */
-    int cpu() const;
-
-    /** Returns the used CPU time for any given time span. */
-    int usedCpu() const;
-
-    /** Returns the amount of RAM installed in the system, in mebibytes */
-    int ram() const;
-
-    /** Returns the amount of RAM that is used by applications, in mebibytes */
-    int usedRam() const;
-
-    /** Returns the amount of swap that is available on this system in MiB */
-    int swap() const;
-
-    /** Returns the amount of swap that is currently being used in MiB */
-    int usedSwap() const;
-
-    /** Returns the size of the first SD card in mebibytes */
-    int sd1() const;
-
-    /** Returns the amount of space that is used up on SD 1 in mebibytes */
-    int usedSd1() const;
-
-    /** Returns the size of the second SD card in mebibytes */
-    int sd2() const;
-
-    /** Returns the amount of space that is used up on SD 2 in mebibytes */
-    int usedSd2() const;
-
-signals:
-    /** This value has changed */
-    void cpuUpdated(int value);
-
-    /** This value has changed */
-    void usedCpuUpdated(int value);
-
-    /** This value has changed */
-    void ramUpdated(int value);
-
-    /** This value has changed */
-    void usedRamUpdated(int value);
-
-    /** This value has changed */
-    void swapUpdated(int value);
-
-    /** This value has changed */
-    void usedSwapUpdated(int value);
-
-    /** This value has changed */
-    void sd1Updated(int value);
-
-    /** This value has changed */
-    void usedSd1Updated(int value);
-
-    /** This value has changed */
-    void sd2Updated(int value);
-
-    /** This value has changed */
-    void usedSd2Updated(int value);
-
-protected slots:
-    void update();
+    static SystemInformationAttached *qmlAttachedProperties(QObject *parent);
 };
 
-QML_DECLARE_TYPE(SystemInformation);
+QML_DECLARE_TYPEINFO(SystemInformation, QML_HAS_ATTACHED_PROPERTIES)
 
 #endif // SYSTEMINFORMATION_H
